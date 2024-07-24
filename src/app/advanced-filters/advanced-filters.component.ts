d53e117a50abf6cfccd723dd936902972cc6e398
Carlos Alvarado <c.alvarado_@hotmail.com>
2024-07-24 15:56:40 -0400

    adding advance-filters component and data-table component

---
+++ src/app/advanced-filters/advanced-filters.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'advanced-filters',
+  standalone: true,
+  imports: [],
+  templateUrl: './advanced-filters.component.html',
+  styleUrl: './advanced-filters.component.css',
+})
+export class AdvancedFiltersComponent {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { ButtonModule } from 'primeng/button';
+import { AdvancedFiltersComponent } from './advanced-filters/advanced-filters.component';
+import { DataTableComponent } from './data-table/data-table.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ButtonModule,
+    AdvancedFiltersComponent,
+    DataTableComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'frontend_pvt';
